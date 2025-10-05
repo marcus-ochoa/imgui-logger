b27a957d4e19c4c59a6d58d701ba5279dd33c21b
marcus-ochoa <marcus_ochoa@proton.me>
2025-10-04 22:16:02 -0700

    Setup project working

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marcus/Documents/School/cmpm123/imgui-logger
-# Build directory: /home/marcus/Documents/School/cmpm123/imgui-logger/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
